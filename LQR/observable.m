d5d5f9b7ba9dc9731d250393657dacc01ab4e1f0
nabnab0 <136686414+nabnab0@users.noreply.github.com>
2023-06-16 17:57:57 +0700

    Add files via upload

---
+++ LQR/observable.m
@@ -0,0 +1,28 @@
+ob = obsv(sys_ss);
+observability = rank(ob)
+
+poles = eig(Ac)
+
+P = [-40 -41 -42 -43];
+L = place(A',C',P)'
+
+Ace = [(A-B*K) (B*K);
+       zeros(size(A)) (A-L*C)];
+Bce = [B*Nbar;
+       zeros(size(B))];
+Cce = [Cc zeros(size(Cc))];
+Dce = [0;0];
+
+states = {'x' 'x_dot' 'phi' 'phi_dot' 'e1' 'e2' 'e3' 'e4'};
+inputs = {'r'};
+outputs = {'x'; 'phi'};
+
+sys_est_cl = ss(Ace,Bce,Cce,Dce,'statename',states,'inputname',inputs,'outputname',outputs);
+
+t = 0:0.01:5;
+r = 0.2*ones(size(t));
+[y,t,x]=lsim(sys_est_cl,r,t);
+[AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot');
+set(get(AX(1),'Ylabel'),'String','cart position (m)')
+set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)')
+title('Step Response with Observer-Based State-Feedback Control')
---
+++ LQR/precompasation.m
@@ -0,0 +1,13 @@
+Cn = [1 0 0 0];
+sys_ss = ss(A,B,Cn,0);
+Nbar = rscale(sys_ss,K)
+
+sys_cl = ss(Ac,Bc*Nbar,Cc,Dc,'statename',states,'inputname',inputs,'outputname',outputs);
+
+t = 0:0.01:5;
+r =0.2*ones(size(t));
+[y,t,x]=lsim(sys_cl,r,t);
+[AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot');
+set(get(AX(1),'Ylabel'),'String','cart position (m)')
+set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)')
+title('Step Response with Precompensation and LQR Control')
