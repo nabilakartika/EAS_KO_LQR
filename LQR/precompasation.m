953104ae74d119cada4b72ef64c38a4974d2006c
nabnab0 <136686414+nabnab0@users.noreply.github.com>
2023-06-16 19:15:01 +0700

    Update precompasation.m

--- LQR/precompasation.m
+++ LQR/precompasation.m
@@ -6,6 +6,7 @@ sys_cl = ss(Ac,Bc*Nbar,Cc,Dc,'statename',states,'inputname',inputs,'outputname',
 
 t = 0:0.01:5;
 r =0.2*ones(size(t));
+figure(3)
 [y,t,x]=lsim(sys_cl,r,t);
 [AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot');
 set(get(AX(1),'Ylabel'),'String','cart position (m)')
